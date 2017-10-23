d918eb41c2d0e0a7ee5c99b61ab3ec7a065353ab
Ricardo Hernandez <sppriga78@gmail.com>
2017-10-23 21:18:31 +0000

    Tareas terminadas

--- src1/tareas/mario_dificil.c
+++ src1/tareas/mario_dificil.c
@@ -1,8 +1,58 @@
-Implementa un programa que muestre una piramide doble como la que se muestra abajo.
+/*Implementa un programa que muestre una piramide doble como la que se muestra abajo.
 
 $ ./mario
 Altura: 4
    #  #
   ##  ##
  ###  ###
-####  ####
+####  ####*/
+
+#include "stdio.h"
+#include "cs50.h"
+
+int limite_piramide ();
+void imp_piramide();
+void imp_espacios (int altitud,int nivel);
+void imp_gatitos(int nivel);
+
+int main(){
+    imp_piramide();
+}
+
+int limite_piramide(){
+    int a;
+    printf("Ingresa el tamanio limite de la piramide: ");
+    a = get_int();
+    while(a < 0 || a >= 20){
+        printf("El numero debe ser positivo y menor a 20, ingrese de nuevo: ");
+        a = get_int();
+    }
+    return a;
+}
+
+void imp_piramide(){
+     int limit;
+    limit = limite_piramide();
+    for(int i=0;i<limit;i++){
+        imp_espacios(limit,i);
+        imp_gatitos(i);
+        printf("  ");
+        imp_gatitos(i);
+        printf("\n");
+    }
+}
+
+void imp_espacios (int altitud, int nivel){
+    for(int k = altitud-nivel-1;k > 0;k--){
+            printf(" ");
+        }
+}
+
+void imp_gatitos (int nivel){
+    for(int l = 0;l < nivel+1;l++){
+            printf("#");
+        }
+}
+
+
+
--- src1/tareas/mario_facil.c
+++ src1/tareas/mario_facil.c
@@ -1,6 +1,6 @@
-Implementa un programa que imprima una media piramide de una altura especificada por el usuario, como abajo:
+/*Implementa un programa que imprima una media piramide de una altura especificada por el usuario, como abajo:
 
-$ ./mario
+$ ./mario_facil
 Altura: 5
     ##
    ###
@@ -8,8 +8,29 @@ Altura: 5
  #####
 ######
 
-$ ./mario
+$ ./mario_facil
 Altura: 3
   ##
  ###
-####
+####*/
+#include "stdio.h"
+#include "cs50.h"
+
+int main(){
+    int limit;
+    printf("Ingresa el tamanio limite de la piramide: ");
+    limit = get_int();
+    while(limit < 0 || limit >= 20){
+        printf("El numero debe ser positivo y menor a 20, ingrese de nuevo: ");
+        limit = get_int();
+    }
+    for(int j = 0;j < limit;j++){
+        for(int k = limit-j-1;k > 0;k--){
+            printf(" ");
+        }
+        for(int l = 0;l < j+2;l++){
+            printf("#");
+        }
+        printf("\n");
+    }
+}
