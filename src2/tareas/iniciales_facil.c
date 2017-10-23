4fbee7877fce06613a36062a0b53c3f787f4611b
Ricardo Hernandez <sppriga78@gmail.com>
2017-10-23 21:23:46 +0000

    Tarea realizada

--- src2/tareas/iniciales_facil.c
+++ src2/tareas/iniciales_facil.c
@@ -1,5 +1,20 @@
-Implemente un programa que, dado el nombre de una persona, imprima las iniciales de la personas, como se muestra abajo.
+/*Implemente un programa que, dado el nombre de una persona, imprima las iniciales de la personas, como se muestra abajo.
 
 $ ./iniciales_facil
 Regulus Arcturus Black
-RAB
+RAB*/
+
+#include "stdio.h"
+#include "cs50.h"
+#include "string.h"
+#include "ctype.h"
+
+int main(void){
+    printf("Nombre: ");
+    string nombre = get_string();
+    for(int i = 0, n = strlen(nombre);i < n; i++){
+        if(isupper(nombre[i])){
+            printf("%c\n",nombre[i]);
+        }
+    }
+}
